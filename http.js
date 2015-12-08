32dcfa34f195ff7156373bef4c4ccdcf861e99c3
sergeyMelentyev <sergey.melentyev@gmail.com>
2015-12-08 18:36:28 +0300

    new version

---
+++ http.js
@@ -0,0 +1,10 @@
+"use strick"
+
+var http = require("http");
+var fs = require("fs");
+
+http.createServer(function(req, res){
+	res.writeHead(200, { "Content-Type": "text/html" });
+	var html = fs.readFileSync(__dirname + "/index.htm");
+	res.end(html);
+}).listen(8080, "127.0.0.1");
