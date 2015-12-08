2bb904262eb5a4c80b47b386f7028bb7f0e7f06e
sergeyMelentyev <sergey.melentyev@gmail.com>
2015-12-08 19:28:45 +0300

    new version

--- http.js
+++ http.js
@@ -7,13 +7,15 @@ http.createServer(function(req, res){
 	if (req.url === "/") {
 		res.writeHead(200, { "Content-Type": "text/html" });
 		fs.createReadStream(__dirname + "/index.htm").pipe(res);	// response with streams (chunks)
-	}
-	if (req.url === "/api") {
+	} else if (req.url === "/api") {
 		res.writeHead(200, { "Content-Type": "application/json" });
 		var obj = {
 			firstName: "Sergey",
 			lastName: "Melentyev"
 		};
 		res.end(JSON.stringify(obj));
+	} else {
+		res.writeHead(404);	// responce for incorrect url
+		res.end();
 	}
 }).listen(8080, "127.0.0.1");
