a756311b94aad15ea5286aa35238d7142d39f84a
sergeyMelentyev <sergey.melentyev@gmail.com>
2015-12-09 13:58:37 +0300

    new version

--- http.js
+++ http.js
@@ -2,21 +2,56 @@
 
 var http = require("http");
 var fs = require("fs");
-var port = process.env.PORT || 8080;	// if server port exist or local host
+var path = require('path');
+var port = process.env.PORT || 8080;
 
-http.createServer(function(req, res){
-	if (req.url === "/") {
-		res.writeHead(200, { "Content-Type": "text/html" });
-		fs.createReadStream(__dirname + "/index.htm").pipe(res);	// response with streams (chunks)
-	} else if (req.url === "/api") {
-		res.writeHead(200, { "Content-Type": "application/json" });
+http.createServer(function (request, response) {
+    var filePath = '.' + request.url;
+    if (filePath === './') {
+		fs.createReadStream(__dirname + "/index.htm").pipe(response);
+    } else if (filePath === "./api") {
 		var obj = {
 			firstName: "Sergey",
 			lastName: "Melentyev"
 		};
-		res.end(JSON.stringify(obj));
-	} else {
-		res.writeHead(404);	// responce for incorrect url
-		res.end();
+		response.end(JSON.stringify(obj));
 	}
+    var extname = path.extname(filePath);
+    var contentType = 'text/html';
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break; 
+        case '.png':
+            contentType = 'image/png';
+            break;      
+        case '.jpg':
+            contentType = 'image/jpg';
+            break;
+    }
+    fs.readFile(filePath, function(error, content) {
+        if (error) {
+            if(error.code == 'ENOENT'){
+                fs.readFile('./404.html', function(error, content) {
+                    response.writeHead(200, { 'Content-Type': contentType });
+                    response.end(content, 'utf-8');
+                });
+            }
+            else {
+                response.writeHead(500);
+                response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+                response.end(); 
+            }
+        }
+        else {
+            response.writeHead(200, { 'Content-Type': contentType });
+            response.end(content, 'utf-8');
+        }
+    });
 }).listen(port);
